ac90568470ac62558d46e6830c075327109426bb
terukusu <teru.kusu@gmail.com>
2018-07-18 16:50:30 +0900

    ソースコードを整形

--- src/com/example/vendingmachine/Main.java
+++ src/com/example/vendingmachine/Main.java
@@ -6,62 +6,62 @@ import java.io.InputStreamReader;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		int[] coinValues = {500, 100, 50, 10};
+		int[] coinValues = { 500, 100, 50, 10 };
 		int[] coinAmmounts = new int[4];
 
 		int totalInputMoney = 0;
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 各貨幣の枚数を入力
-        for (int i = 0; i < coinValues.length; i++) {
-        	// 対象の貨幣
-        	int currentValue = coinValues[i];
-        	
-        	System.out.print(currentValue + "?: ");
-            int ammount = Integer.parseInt(br.readLine());
+		// 各貨幣の枚数を入力
+		for (int i = 0; i < coinValues.length; i++) {
+			// 対象の貨幣
+			int currentValue = coinValues[i];
 
-            // 枚数チェック
-            if (ammount < 0) {
-                System.out.println("minus");
-                return;
-            }
- 
-            // 枚数を記録
-            coinAmmounts[i] = ammount;
+			System.out.print(currentValue + "?: ");
+			int ammount = Integer.parseInt(br.readLine());
 
-            // トータル入力金額に加算
-            totalInputMoney += currentValue * ammount;
-        }
+			// 枚数チェック
+			if (ammount < 0) {
+				System.out.println("minus");
+				return;
+			}
 
-        System.out.println("total input money: " + totalInputMoney);
+			// 枚数を記録
+			coinAmmounts[i] = ammount;
 
-        // 入力ストリーム開放
-        br.close();
+			// トータル入力金額に加算
+			totalInputMoney += currentValue * ammount;
+		}
 
-        // お釣り計算
-        int changeTotal = totalInputMoney - 120;
-        int changeRemain = changeTotal;
+		System.out.println("total input money: " + totalInputMoney);
 
-        System.out.println("change total: " + changeTotal);
+		// 入力ストリーム開放
+		br.close();
 
-        int[] changeAmmounts = new int[4];
+		// お釣り計算
+		int changeTotal = totalInputMoney - 120;
+		int changeRemain = changeTotal;
 
-        // 各貨幣の枚数を入力
-        for (int i = 0; i < coinValues.length; i++) {
-        	// 対象の貨幣
-        	int currentValue = coinValues[i];
-        	
-            // 貨幣の枚数計算
-            int ammount = changeRemain / currentValue;
-            changeRemain -= ammount * currentValue;
+		System.out.println("change total: " + changeTotal);
 
-            // 枚数を記録
-            changeAmmounts[i] = ammount;
+		int[] changeAmmounts = new int[4];
 
-            System.out.println("change " + currentValue + ": " + ammount);
-        }
+		// 各貨幣の枚数を入力
+		for (int i = 0; i < coinValues.length; i++) {
+			// 対象の貨幣
+			int currentValue = coinValues[i];
 
-        System.out.println("change remain: " + changeRemain);
+			// 貨幣の枚数計算
+			int ammount = changeRemain / currentValue;
+			changeRemain -= ammount * currentValue;
+
+			// 枚数を記録
+			changeAmmounts[i] = ammount;
+
+			System.out.println("change " + currentValue + ": " + ammount);
+		}
+
+		System.out.println("change remain: " + changeRemain);
 	}
 }
